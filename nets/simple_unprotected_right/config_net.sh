d4afc8729868512048b53d7605fcca7e8e0eba3d
Finn Kaiser <37416935+Finnk11@users.noreply.github.com>
2024-08-15 14:33:00 +0200

    traffic light phases in accordance to sumo_rl requirements

--- nets/simple_unprotected_right/config_net.sh
+++ nets/simple_unprotected_right/config_net.sh
@@ -1,4 +1,5 @@
 netconvert --node-files=netconfig/nodes.nod.xml \
   --edge-files=netconfig/edges.edg.xml \
   --connection-files=netconfig/connections.con.xml  \
+  --tllogic-files=netconfig/tllogics.tll.xml \
   --output-file=simple_unprotected_right.net.xml
