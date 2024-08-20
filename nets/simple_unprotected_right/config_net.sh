99ebab7e3487621b4e2001ec001d0b2d69bb4a06
Finn Kaiser <37416935+Finnk11@users.noreply.github.com>
2024-08-20 17:52:08 +0200

    automatic generation of sumo config file

--- nets/simple_unprotected_right/config_net.sh
+++
@@ -1,5 +0,0 @@
-netconvert --node-files=netconfig/nodes.nod.xml \
-  --edge-files=netconfig/edges.edg.xml \
-  --connection-files=netconfig/connections.con.xml  \
-  --tllogic-files=netconfig/tllogics.tll.xml \
-  --output-file=simple_unprotected_right.net.xml
