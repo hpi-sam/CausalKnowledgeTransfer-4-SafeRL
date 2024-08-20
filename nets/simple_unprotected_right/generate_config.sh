291f4ecab5ca93db5ca8f89c9b00a362f35ab68d
Finn Kaiser <37416935+Finnk11@users.noreply.github.com>
2024-08-20 18:56:53 +0200

    prevent regeneration of files that don't change

--- nets/simple_unprotected_right/generate_config.sh
+++ nets/simple_unprotected_right/generate_config.sh
@@ -1,7 +1,14 @@
 END_TIME=3600
 REPEAT_PERIOD=10
 
-netconvert -c simple_unprotected_right.netccfg
+#netconvert \
+#  --node-files=netconfig/nodes.nod.xml \
+#  --edge-files=netconfig/edges.edg.xml \
+#  --connection-files=netconfig/connections.con.xml  \
+#  --tllogic-files=netconfig/tllogics.tll.xml \
+#  --output-file=simple_unprotected_right.net.xml
+
+netconvert --configuration-file simple_unprotected_right.netccfg
 
 python $SUMO_HOME/tools/findAllRoutes.py \
   -n simple_unprotected_right.net.xml \
@@ -9,26 +16,31 @@ python $SUMO_HOME/tools/findAllRoutes.py \
   -s southJunction,westJunction \
   -t junctionEast,junctionNorth
 
-duarouter -c simple_unprotected_right.duarcfg
+#duarouter \
+#  --net-file simple_unprotected_right.net.xml \
+#  --route-files vconfig/vtypes.rou.xml,routes.rou.xml,vconfig/vehicles.rou.xml \
+#  --output-file config.rou.xml
+
+duarouter --configuration-file simple_unprotected_right.duarcfg
 
 python $SUMO_HOME/tools/route/vehicle2flow.py config.rou.xml \
   -o simple_unprotected_right.rou.xml \
   -e $END_TIME -r $REPEAT_PERIOD
 
-sumo \
-  --net-file simple_unprotected_right.net.xml \
-  --route-files simple_unprotected_right.rou.xml \
-  --output-prefix "output/" \
-  --statistic-output statistics.xml \
-  --collision-output collisions.xml \
-  --device.ssm.probability 1 \
-  --device.ssm.file ssm.xml \
-  --collision.action "teleport" \
-  --collision.check-junctions true \
-  --default.speeddev 0.1 \
-  --random \
-  --save-configuration simple_unprotected_right.sumocfg \
-  --save-commented true
+#sumo \
+#  --net-file simple_unprotected_right.net.xml \
+#  --route-files simple_unprotected_right.rou.xml \
+#  --output-prefix "output/" \
+#  --statistic-output statistics.xml \
+#  --collision-output collisions.xml \
+#  --device.ssm.probability 1 \
+#  --device.ssm.file ssm.xml \
+#  --collision.action "teleport" \
+#  --collision.check-junctions true \
+#  --default.speeddev 0.1 \
+#  --random \
+#  --save-configuration simple_unprotected_right.sumocfg \
+#  --save-commented true
 
 mkdir -p output
 
