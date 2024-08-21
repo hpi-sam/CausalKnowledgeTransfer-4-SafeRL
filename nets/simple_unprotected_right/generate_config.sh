7a0ee6d727f11576fd48bb8978a09b148b3ccda6
Finn Kaiser <37416935+Finnk11@users.noreply.github.com>
2024-08-21 14:05:06 +0200

    variable friction for later experiments

--- nets/simple_unprotected_right/generate_config.sh
+++ nets/simple_unprotected_right/generate_config.sh
@@ -1,5 +1,6 @@
 END_TIME=3600
 REPEAT_PERIOD=10
+FRICTION="0.5"
 
 #netconvert \
 #  --node-files=netconfig/nodes.nod.xml \
@@ -8,7 +9,8 @@ REPEAT_PERIOD=10
 #  --tllogic-files=netconfig/tllogics.tll.xml \
 #  --output-file=simple_unprotected_right.net.xml
 
-netconvert --configuration-file simple_unprotected_right.netccfg
+netconvert --configuration-file simple_unprotected_right.netccfg \
+--default.friction $FRICTION
 
 python $SUMO_HOME/tools/findAllRoutes.py \
   -n simple_unprotected_right.net.xml \
@@ -27,20 +29,21 @@ python $SUMO_HOME/tools/route/vehicle2flow.py config.rou.xml \
   -o simple_unprotected_right.rou.xml \
   -e $END_TIME -r $REPEAT_PERIOD
 
-#sumo \
-#  --net-file simple_unprotected_right.net.xml \
-#  --route-files simple_unprotected_right.rou.xml \
-#  --output-prefix "output/" \
-#  --statistic-output statistics.xml \
-#  --collision-output collisions.xml \
-#  --device.ssm.probability 1 \
-#  --device.ssm.file ssm.xml \
-#  --collision.action "teleport" \
-#  --collision.check-junctions true \
-#  --default.speeddev 0.1 \
-#  --random \
-#  --save-configuration simple_unprotected_right.sumocfg \
-#  --save-commented true
+sumo \
+  --net-file simple_unprotected_right.net.xml \
+  --route-files simple_unprotected_right.rou.xml \
+  --output-prefix "output/" \
+  --statistic-output statistics.xml \
+  --collision-output collisions.xml \
+  --device.ssm.probability 1 \
+  --device.ssm.file ssm.xml \
+  --device.ssm.measures "BR" \
+  --collision.action "teleport" \
+  --collision.check-junctions true \
+  --default.speeddev 0.1 \
+  --random \
+  --save-configuration simple_unprotected_right.sumocfg \
+  --save-commented true
 
 mkdir -p output
 
