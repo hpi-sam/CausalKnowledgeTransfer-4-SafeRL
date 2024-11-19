35aae4f34bf56ef953f92abd691cab0ba4f9f3b8
Finn Kaiser <37416935+Finnk11@users.noreply.github.com>
2024-11-19 20:54:11 +0100

    generate traces sh script

--- env/SumoTraceGenerator.py
+++ env/SumoTraceGenerator.py
@@ -20,7 +20,7 @@ class SumoTraceGenerator:
             friction_coefficient = max(0.01, min(1, friction_coefficient))
             experiment_string = str(experiment).zfill(4)
             env = env_generator.get_generation_env(output_prefix=str(path.joinpath(experiment_string)))
-            model = A2C(env=env, policy='MlpPolicy').load(Path().joinpath('env', 'training_data', 'a2c'))
+            model = A2C(env=env, policy='MlpPolicy').load(Path().joinpath('env', 'training_data_2lane', 'a2c_alternating_server'))
 
             metadata = {'desiredSpeed': speed, 'friction': friction_coefficient}
             pd.DataFrame(metadata, index=['metadata']).to_xml(Path(path).joinpath(experiment_string + '_metadata.xml'))
@@ -51,12 +51,42 @@ def main():
     from tqdm import tqdm
     import itertools
 
+    print("Begin training configuration")
+    import xml.etree.ElementTree as ET
+
+    INSERT_PROBABILITY = 0.1
+
+    config_directory = Path().joinpath('nets', '2lane_unprotected_right')
+    route_file = config_directory.joinpath('2lane_unprotected_right.rou.xml')
+    tree = ET.parse(route_file)
+    root = tree.getroot()
+
+    for flow in root.findall('flow'):
+        match flow.attrib.get('id'):
+            case 'southEast':
+                flow.set('period', f"exp({INSERT_PROBABILITY})")
+            case 'southNorth':
+                flow.set('period', f"exp({INSERT_PROBABILITY})")
+            case 'westEastTop':
+                flow.set('period', f"exp({2 * INSERT_PROBABILITY})")
+            case id if 'westEastBottom' in id:
+                flow.set('end', str(float(flow.get('begin')) + 600))
+                if float(flow.get('begin')) % 1200 == 0:
+                    flow.set('period', f"exp({2 * INSERT_PROBABILITY})")
+                else:
+                    flow.set('period', f"exp({0.0001 * INSERT_PROBABILITY})")
+
+    tree.write(route_file, xml_declaration=True, encoding='UTF-8')
+    print("Finished training configuration")
+
+    net_name = '2lane_unprotected_right'
+
     environments = SumoEnvironmentGenerator(
-        net_file=str(Path().joinpath('nets', 'simple_unprotected_right', 'simple_unprotected_right.net.xml')),
-        route_file=str(Path().joinpath('nets', 'simple_unprotected_right', 'simple_unprotected_right.rou.xml')),
-        sumocfg_file=str(Path().joinpath('nets', 'simple_unprotected_right', 'simple_unprotected_right.sumocfg')),
+        net_file=str(Path().joinpath('nets', net_name, f'{net_name}.net.xml')),
+        route_file=str(Path().joinpath('nets', net_name, f'{net_name}.rou.xml')),
+        sumocfg_file=str(Path().joinpath('nets', net_name, f'{net_name}.sumocfg')),
         duration=3600,
-        learning_data_csv_name=str(Path().joinpath('env', 'training_data', 'output.csv')),
+        learning_data_csv_name=str(Path().joinpath('env', 'training_data_2lane', 'output.csv')),
     )
 
     speeds = [30, 50, 80, 100, 130]
@@ -65,7 +95,7 @@ def main():
     experiments = list(itertools.product(speeds, frictions))
 
     for speed, friction in tqdm(experiments):
-        simulation_output_path = Path().joinpath('data', f'a2c_{int(speed)}_f{friction}')
+        simulation_output_path = Path().joinpath('data_agent', f'a2c_{int(speed)}_f{friction}')
         Path.mkdir(simulation_output_path, parents=True, exist_ok=True)
 
         trace_generator = SumoTraceGenerator()
