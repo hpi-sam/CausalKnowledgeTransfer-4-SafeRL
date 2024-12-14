1ff3d0b683bc202851551c4d917ac6e687dd290e
Finn Kaiser <37416935+Finnk11@users.noreply.github.com>
2024-12-14 20:44:32 +0100

    paper training

--- env/train.py
+++ env/train.py
@@ -11,7 +11,7 @@ if not sumo_home:
         "SUMO_HOME environment variable is not set. Please set it to your SUMO installation directory.")
 
 # Constants / Parameters
-SPEED = 8.33
+SPEED = 13.89
 FRICTION = 1.0
 INSERT_PROBABILITY = 0.1
 DURATION = 3600
@@ -123,21 +123,20 @@ print("Finished initiating environment")
 
 print(f"Begin training with Speed {SPEED} and friction {FRICTION}")
 from stable_baselines3.a2c import A2C
+for model_name in ['scratch_s50_f1', 'scratch_s50_f0.5']:
+  env = environments.get_training_env()
+  model = A2C(
+      env=env,
+      policy='MlpPolicy',
+      n_steps=100,
+      verbose=1,
+      tensorboard_log='tensorboard_paper'
+  )
 
-env = environments.get_training_env()
-model = A2C(
-    env=env,
-    policy='MlpPolicy',
-    n_steps=100,
-    verbose=1,
-    tensorboard_log='tensorboard'
-)
+  model = model.load(Path().joinpath('env', 'agents_paper', 'warmup_agent.zip'), env=env, tensorboard_log='tensorboard_paper')
 
-# model = A2C.load(Path().joinpath('env', 'training_data_2lane', 'a2c_alternating_server.zip'))
-# model.tensorboard_log = 'tensorboard'
-# model.set_env(env)
+  print(model.policy, model.n_steps, model.verbose, model.tensorboard_log)
 
-model_name = 'a2c_s8.33_f1.0'
-model.learn(1_000_000, tb_log_name=model_name)
-model.save(Path().joinpath('env', 'training_data_2lane', model_name))
+  model.learn(1_000_000, tb_log_name=model_name)
+  model.save(Path().joinpath('env', 'agents_paper', model_name + '.zip'))
 print("Finished training")
